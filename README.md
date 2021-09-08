8cd3ab9298816e1141bb28b2ae02a10a68130263
Brice Schaffner <brice.schaffner@swisstopo.ch>
2021-09-08 11:13:05 +0200

    wip

---
+++ README.md
@@ -0,0 +1,13 @@
+# Bump tag with milestone
+
+TODO
+
+
+## Outputs
+
+## `new_tag`
+
+New tag
+
+## Example usage
+
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { PullRequestWebhookPayload } from './interfaces';
+
+
+async function run() {
+    const pullRequest = (github.context.payload as PullRequestWebhookPayload).pull_request;
+    if (!pullRequest) {
+        core.setFailed('Could not get pull_request from context, exiting');
+        return;
+    }
+    core.debug(`pull request payload: ${pullRequest}`)
+
+}
+
+run().catch((error) => {
+    core.setFailed(error.message);
+  });
